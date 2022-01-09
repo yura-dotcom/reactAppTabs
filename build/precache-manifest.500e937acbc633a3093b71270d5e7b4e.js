538d61a3e631167da6b570e485cc85d1d8bb2701
master <devgeorg222@gmail.com>
2022-01-09 23:36:25 +0200

    second

---
+++ build/precache-manifest.500e937acbc633a3093b71270d5e7b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8489d320e4e4dbb4f871f81135db2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edce7668701984b4355c",
+    "url": "/static/css/main.8cf08574.chunk.css"
+  },
+  {
+    "revision": "a92dad93df825662b7a4",
+    "url": "/static/js/2.a45a6fe3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a45a6fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edce7668701984b4355c",
+    "url": "/static/js/main.faba4bd7.chunk.js"
+  },
+  {
+    "revision": "e5d628ff18d24e18b597",
+    "url": "/static/js/runtime-main.af0cddbb.js"
+  }
+]);
